dfe5f7b5f6730d8a277e9a63f3a9484cc36ff7f8
Coder-Bushrakhan <bk.bushra555@gmail.com>
2023-11-29 22:46:38 +0500

    commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,7 @@
 export default function Contact()
 {
  
-  const handleClick = () => {
-
-      alert("Thanks for response");
-    
   
-    }
-    // Add your code here
  
     return(
 <div>
@@ -67,7 +61,7 @@ export default function Contact()
         <div className="p-2 w-full">
         <button
                               className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                              onClick={handleClick} // Add the onClick attribute to the button element
+          
                             >
                             Submit!
                             </button>
